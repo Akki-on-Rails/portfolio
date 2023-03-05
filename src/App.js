6cf9f8a2d5fe0b09e93cf3bcc45f259d9395e262
Frank Gerngross <f.gerngross@googlemail.com>
2023-03-05 13:29:16 +0100

    connect background component to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import Background from './components/background';
 import styles from './styles/App.scss';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <header className={styles.App_header}>
-        <img src={logo} className={styles.App_logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.App_link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body>
+        <Background />
+      </body>
     </div>
   );
 }

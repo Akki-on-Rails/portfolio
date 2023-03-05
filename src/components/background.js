b98a3546b012ef3ff8f31ef07d59a0ff8ee81d3c
Frank Gerngross <f.gerngross@googlemail.com>
2023-03-05 12:22:54 +0100

    create background component

---
+++ src/components/background.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from '../styles/components/background.module.scss';
+
+const Background = () => {
+  return (
+    <div className={styles.logoWrapper}>
+      <div className={styles.lineOne}>code+</div>
+      <div className={styles.lineTwo}>design</div>
+    </div>
+  )
+}
+
+export default Background

e1fe9476a76acc8ecb88ad67844daff1298fe422
Frank Gerngross <f.gerngross@googlemail.com>
2023-03-06 15:54:13 +0100

    add general shadow effect to project. also add gradient instead line to divide components

--- src/components/worksGalery.js
+++ src/components/worksGalery.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import '../styles/main.scss';
+import '../styles/atoms/shadowEffects.scss';
 
 const WorksGalery = () => {
   return (
     <div className='galery-wrapper'>
       <h2>Projects</h2>
       <div className='element-container'>
+        {/* TO DO: only first class is being used
+        to style the div. WHY??? */}
+        <div className='galery-element effect5 test'></div>
         <div className='galery-element'></div>
         <div className='galery-element'></div>
         <div className='galery-element'></div>

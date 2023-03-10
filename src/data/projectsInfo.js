f806ba71a86d9003e00cceda2300580fd0b44700
Frank Gerngross <f.gerngross@googlemail.com>
2023-03-10 11:09:55 +0100

    create a js data file with content for the project cards.

---
+++ src/data/projectsInfo.js
@@ -0,0 +1,29 @@
+export const project = [
+  {
+    name: 'BoxTag',
+    id: 1,
+    image: ['../images/BoxTag-Mockup.jpg'],
+    alt: ['BoxTag App on different mobile screens'],
+    short: 'This PWA helps you manage all your stuff and boxes',
+    long: `Manage boxes and their insides. Print QR labels to find
+          the right box and to see whats inside without opening it.
+          Also you can share your box collections with others e.g.
+          when moving all your helpers can scan the boxes and see,
+          where to put it.
+          The app was realised with Ruby on Rails. Tricky features
+          where e.g. to use the device's camera to scan the QR codes
+          and to make the app a PWA.`,
+    link: ['https://github.com/Akki-on-Rails/boxtag'],
+    techstack: ['Ruby', 'Ruby on Rails', 'HTML', 'CSS', 'SCSS', 'Heroku']
+  },
+  {
+    name: 'SkillBridge',
+    id: 2,
+    image: [],
+    alt: [],
+    short: 'write a one-liner about the project',
+    long: 'give a long description for the project',
+    link: ['https://github.com/Akki-on-Rails/skill-bridge'],
+    techstack: ['React', 'NodeJS', 'MongoDB', 'JavaScript', 'HTML', 'CSS', 'SCSS']
+  }
+]

91cde609f37b866c899c595a54a7331760a8d3f1
Frank Gerngross <f.gerngross@googlemail.com>
2023-03-13 10:34:33 +0100

    fix side menu to make it sticky

--- src/components/sideMenu.js
+++ src/components/sideMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import '../styles/main.scss';
 import "../assets/css/fontawesome.css";
@@ -7,15 +7,17 @@ import "../assets/css/solid.css";
 
 const SideMenu = () => {
   return (
-    <div className='sideMenu-wrapper'>
-      <a href='mailto:f.gerngross@googlemail.com'>
-        <i class="fa-sharp fa-solid fa-envelope"></i>
-        <h6>f.gerngross@googlemail.com</h6>
-      </a>
-      <a href='https://github.com/Akki-on-Rails' target='_blank'>
-        <i class="fa-brands fa-github"></i>
-        <h6>Akki-on-Rails</h6>
-      </a>
+    <div className='sideMenu-container'>
+      <div className='link-wrapper'>
+        <a href='mailto:f.gerngross@googlemail.com'>
+          <i class="fa-sharp fa-solid fa-envelope"></i>
+          <h6>f.gerngross@googlemail.com</h6>
+        </a>
+        <a href='https://github.com/Akki-on-Rails' target='_blank'>
+          <i class="fa-brands fa-github"></i>
+          <h6>Akki-on-Rails</h6>
+        </a>
+      </div>
     </div>
   )
 }

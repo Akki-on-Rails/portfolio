f8b325baae15aa5394cccdeeb49b5cef5071e79a
Frank Gerngross <f.gerngross@googlemail.com>
2023-03-14 11:45:51 +0100

    simplify text truncate function

--- src/functions/textTruncate.js
+++ src/functions/textTruncate.js
@@ -1,5 +1,8 @@
 export function textTruncate(text, num) {
   return (
-    (text.length <= num) ? text : text.slice(0, num) + '...'
+    text.slice(0, Math.min(text.length, num)) + '...'
+
+    // a longer version would be:
+    // (text.length <= num) ? text : text.slice(0, num) + '...'
   )
 }

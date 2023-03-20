710830bd80a18bd36f256a87bab0ae67dfca812d
Frank Gerngross <f.gerngross@googlemail.com>
2023-03-20 10:48:36 +0100

    change style for mobile view cards

--- src/components/worksGalery.js
+++ src/components/worksGalery.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import '../styles/main.scss';
-import project1 from '../images/BoxTag-Mockup.jpg';
 import { Project } from '../data/projectsInfo'
 
 import { textTruncate } from '../functions/textTruncate';
@@ -14,16 +13,17 @@ const WorksGalery = () => {
     <div className='galery-wrapper'>
       <h2>Projects</h2>
       <div className='card-wrapper'>
-        <div className='card card-shadow'>
+        <div className='card'>
+          {/* for card shadow add 'card-shadow' to className */}
           <img src={Project[0].image[0]} alt={Project[0].alt}></img>
           <div className='card-info'>
             <h4>{Project[0].name}</h4>
-            {/* for the short text add a conditional to clip the text: */}
+            {/* for the short text added a conditional to clip the text: */}
             {/* textTruncate(text, num) where num ist the max. number of characters */}
             <p>{textTruncate(Project[0].short, 70)}</p>
           </div>
         </div>
-        <div className='card card-shadow'>
+        <div className='card'>
           <img src={Project[1].image[0]} alt={Project[1].alt}></img>
           <div className='card-info'>
             <h4>{Project[1].name}</h4>
